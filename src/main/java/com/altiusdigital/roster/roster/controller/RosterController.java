303fbc5ca21a2b76ae76d69822ffb30f8ac5ecde
Sankararaman Vedagiri <shnkar007@hotmail.com>
2022-04-08 17:45:50 +0530

    Modified the IP in the URL

--- src/main/java/com/altiusdigital/roster/roster/controller/RosterController.java
+++ src/main/java/com/altiusdigital/roster/roster/controller/RosterController.java
@@ -35,57 +35,54 @@ public class RosterController {
     IRosterRepo repo;
     //ResponseEntity<Acceptance> acceptanceResponse = new ResponseEntity<Acceptance>();
     private Object ResponseEntity;
-    @Autowired(required =true)
+    @Autowired(required = true)
     EurekaClient client;
    /* @Value("${concierge.serviceId}")
     private String conciergeId;*/
 
-    @PostMapping(path="/roster",consumes="application/json")
-    public String createRosterEntry(@RequestBody Roster roster){
-        try{
-            repo.save(roster);
-        }catch(JpaSystemException jpse){
-          jpse.getMessage();
+    @PostMapping(path = "/roster", consumes = "application/json")
+    public String createRosterEntry(@RequestBody Roster roster) {
+        try {
+            repo.save (roster);
+        } catch (JpaSystemException jpse) {
+            jpse.getMessage ( );
         }
         return "roster created successfully";
     }
 
-    @GetMapping(path="/roster", produces="application/json")
-    public Roster viewRoster(@RequestParam(value="firstName",required = true) String firstName,@RequestParam(value="date") String date){
-    Roster roster = repo.fetchRosterByFirstNameAndDate (firstName,date);
-        if(roster!=null){
-            System.out.println(roster.getLastName ());
-        }else{
-            System.out.println("roster is null");
+    @GetMapping(path = "/roster", produces = "application/json")
+    public Roster viewRoster(@RequestParam(value = "firstName", required = true) String firstName, @RequestParam(value = "date") String date) {
+        Roster roster = repo.fetchRosterByFirstNameAndDate (firstName, date);
+        if (roster != null) {
+            System.out.println (roster.getLastName ( ));
+        } else {
+            System.out.println ("roster is null");
         }
         return roster;
-       }
-       @PostMapping(path="/accept",consumes="application/json")
-       public ResponseEntity<BookingRequest> acceptRoster(@RequestBody BookingRequest request){
-          bookingRequest = request;
-           ResponseEntity<BookingRequest> booked=null;
-          Application app = client.getApplication ("concierge");
+    }
+
+    @PostMapping(path = "/accept", consumes = "application/json")
+    public ResponseEntity<BookingRequest> acceptRoster(@RequestBody BookingRequest request) {
+        bookingRequest = request;
+        ResponseEntity<BookingRequest> booked = null;
+          /*Application app = client.getApplication ("concierge");
           if(app==null){
               System.out.println("App object is null");
           }
-          InstanceInfo instanceInfo = app.getInstances ().get(0);
-          System.out.println("HostName:  "+instanceInfo.getHostName ());
+         // InstanceInfo instanceInfo = app.getInstances ().get(0);
+        //  System.out.println("HostName:  "+instanceInfo.getHostName ());
           //String url= "http://"+instanceInfo.getHostName ()+":"+instanceInfo.getAppName ()+":"+instanceInfo.getPort ()+"/"+"booking";
-           String url= "http://"+instanceInfo.getHostName ()+":"+instanceInfo.getPort ()+"/booking";
-           System.out.println("VIP Address"+instanceInfo.getVIPAddress ());
-           System.out.println(instanceInfo.getId ());
-          System.out.println("URL "+url);
-          try {
-              System.out.println("TRYYYY");
-
-           booked = restTemplate.postForEntity (new URL (url).toString(), bookingRequest, BookingRequest.class);
-              System.out.println ("Rest Template ");
-          }catch(Exception e){
-              System.out.println("Inside Exception ");
-              e.printStackTrace ();
-          }
-          return booked;
-          //return new ResponseEntity<BookingRequest>(bookingRequest, HttpStatus.CREATED);
-       }
+         //  String url= "http://"+instanceInfo.getHostName ()+":"+instanceInfo.getPort ()+"/booking";
+*/        String url = "http://192.168.65.2:8083/booking";
+        try {
+            System.out.println ("TRYYYY");
+            booked = restTemplate.postForEntity (new URL (url).toString ( ), bookingRequest, BookingRequest.class);
+            System.out.println ("Rest Template ");
+        } catch (Exception e) {
+            System.out.println ("Inside Exception ");
+            e.printStackTrace ( );
+        }
+        return booked;
+    }
 
 }
